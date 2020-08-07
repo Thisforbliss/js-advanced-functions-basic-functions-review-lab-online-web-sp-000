1c8fea0c8b7597f22b05246805c343fff069c24f
Malcolm Leake <echelon1089@gmail.com>
2020-08-07 21:19:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// Your code here
 
-function saturdayFun(activity = "roller-skate"){
-  return console.log(`This Saturday, I want to ${activity}!`)
+const saturdayFun = (arg = 'roller-skate') => {
+    return `This Saturday, I want to ${arg}!`
 }
-saturdayFun(activity)
+
+
+function mondayWork(todo = "go to the office"){
+    return `This Monday, I will ${todo}.`
+}
+
+const wrapAdjective = (wrapper = "*") => {
+    return function(arg = 'special'){
+        return `You are ${wrapper}${arg}${wrapper}!`
+    }
+}
+
+const Calculator = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b, 
+    divide: (a,b) => a / b
+}
+
+const actionApplyer = (int, fns) => {
+    let currVal = int
+    console.log(fns)
+    for(let fn of fns){
+        currVal = fn(currVal)
+    }
+    return currVal
+}
